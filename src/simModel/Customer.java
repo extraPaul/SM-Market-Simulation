64445551de5e696ccac7a3a200b9f99ea97997f6
smndnv <sdane057@uottawa.ca>
2016-11-17 17:50:40 -0500

    Edited class: Customer.java

--- src/simModel/Customer.java
+++ src/simModel/Customer.java
@@ -1,9 +1,14 @@
 package simModel;
 
 public class Customer {
-	
-	
-	
-	
-	
+	// Time a customer enters the store
+	double startWaitTime;
+	// M = Meat & Fish; D = Deli; MD = Both counters
+	enum Type {M, D, MD};
+	// Type of a customer
+	Type uType;
+	// canLeave indicates whether a customer will leave after being served at a counter
+	boolean canLeave = true;
+	// The wait time at which the customer will be dissatisfied
+	double dissatisfactionThreshold;
 }
