a82aca30382c8dfd3f546ce2fce0c962c3ecf071
qufeichen <qufei.chen@gmail.com>
2016-11-17 19:59:19 -0500

    renamed halfHourSchedule to uTotalEmployees

--- src/simModel/EmployeesInfo.java
+++ src/simModel/EmployeesInfo.java
@@ -4,7 +4,7 @@ public class EmployeesInfo {
 
 	// Attributes
 	protected int[][] schedule;
-	protected int[] halfHourSchedule;
+	protected int[] uTotalEmployees;
 	
 	protected int numEmpCleaning;
 	protected int halfHourNumServed;
@@ -19,17 +19,17 @@ public class EmployeesInfo {
 		this.halfHourNumDissatisfied = 0;
 		
 		// derive halfHourSchedule from schedule
-		this.halfHourSchedule = new int[18];
+		this.uTotalEmployees = new int[18];
 		
 		for (int i = 0; i < schedule.length; i++) {
 			int startTime = schedule[i][0];
 			int durationOfShift = schedule[i][1];
 			
-			halfHourSchedule[startTime/30]++;
+			uTotalEmployees[startTime/30]++;
 			
 			int tempCounter = 1;
 			while (durationOfShift > 0) {
-				halfHourSchedule[(startTime/30) + tempCounter]++;
+				uTotalEmployees[(startTime/30) + tempCounter]++;
 				tempCounter++;
 				durationOfShift -= 30;
 			}
