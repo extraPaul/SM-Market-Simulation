4eef4b8b15cb47300c802dac3badfacdc2818230
extraPaul <pllplnt2@gmail.com>
2016-11-24 17:36:32 -0500

    Added UDP for initializing uTotalEmployees

--- src/simModel/EmployeesInfo.java
+++ src/simModel/EmployeesInfo.java
@@ -18,22 +18,7 @@ public class EmployeesInfo {
 		this.halfHourNumServed = 0;
 		this.halfHourNumDissatisfied = 0;
 		
-		// derive halfHourSchedule from schedule
-		this.uTotalEmployees = new int[18];
 		
-		for (int i = 0; i < schedule.length; i++) {
-			int startTime = schedule[i][0];
-			int durationOfShift = schedule[i][1];
-			
-			uTotalEmployees[startTime/30]++;
-			
-			int tempCounter = 1;
-			while (durationOfShift > 0) {
-				uTotalEmployees[(startTime/30) + tempCounter]++;
-				tempCounter++;
-				durationOfShift -= 30;
-			}
-		}
 		
 	}
 	
