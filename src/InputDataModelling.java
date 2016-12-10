0c638b3c1de1ebbe4a9e87b9f9279072da1d7f87
extraPaul <pllplnt2@gmail.com>
2016-12-10 01:30:13 -0500

    Made it print to table

--- src/InputDataModelling.java
+++ src/InputDataModelling.java
@@ -1,7 +1,12 @@
 import java.io.FileNotFoundException;
 import java.io.PrintStream;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
+import wagu.Block;
+import wagu.Board;
+import wagu.Table;
 import cern.jet.random.Exponential;
 import cern.jet.random.engine.MersenneTwister;
 import cern.jet.random.engine.RandomSeedGenerator;
@@ -9,8 +14,8 @@ import cern.jet.random.engine.RandomSeedGenerator;
 
 public class InputDataModelling 
 {
-	public final static int NUM_OBERVATIONS = 30;
-	public final static int OBSERVATION_INTERVAL = NUM_OBERVATIONS * 30;  // observation interval, for each half hour block
+	public final static int NUM_OBSERVATIONS = 30;
+	public final static int OBSERVATION_INTERVAL = NUM_OBSERVATIONS * 30;  // observation interval, for each half hour block
 	// Exponential means for interarrival times for components A and B
 	public final static double[] MEAN_INTER_ARRVL_NORM = {6, 2.4, 2, 2, 1.71, 1.33, 0.92, 1, 1.09, 1.5, 1.71, 1.71, 1.5, 1.33, 1.2, 1};  // minutes
 	public final static double[] MEAN_INTER_ARRVL_DELI = {4, 1, 1.09, 2, 4};  // minutes NOTE: Starts at 11:00 and goes till 1:30
@@ -40,6 +45,11 @@ public class InputDataModelling
 		int normalCount=0, bothCount = 0, deliCount=0; // count number of A and B components
 		double normalArrival, deliArrival;  // For arrivals of components
 		MersenneTwister randGen = new MersenneTwister();
+		
+		double[] prcNorm = new double[16];
+		double[] prcNormBoth = new double[16];
+		double[] prcNormSingle = new double[16];
+		double[] prcDeli = new double[16];
 		for(int i = 0; i < 16; i++){
 			// First arrival is from t = 0
 			normalArrival = interArrival_Normal[i].nextDouble();
@@ -69,11 +79,15 @@ public class InputDataModelling
 					deliCount++;
 				}
 			}
-			System.out.printf("The percentage of normal arrivals = %f %% (normalCount = %d)\n", (double)normalCount/(normalCount+deliCount), normalCount);
-			System.out.printf("The percentage of normal arrivals visiting one counter = %f %% (Count = %d)\n", (double)(normalCount-bothCount)/(normalCount+deliCount), normalCount-bothCount);
-			System.out.printf("The percentage of normal arrivals visiting both counters = %f %% (bothCount = %d)\n", (double)bothCount/(normalCount+deliCount), bothCount);
+			prcNorm[i] = (double)normalCount/(normalCount+deliCount);
+			System.out.printf("The percentage of normal arrivals = %f %% (normalCount = %d)\n", prcNorm[i], normalCount);
+			prcNormSingle[i] = (double)(normalCount-bothCount)/(normalCount+deliCount);
+			System.out.printf("The percentage of normal arrivals visiting one counter = %f %% (Count = %d)\n", prcNormSingle[i], normalCount-bothCount);
+			prcNormBoth[i] = (double)bothCount/(normalCount+deliCount);
+			System.out.printf("The percentage of normal arrivals visiting both counters = %f %% (bothCount = %d)\n", prcNormBoth[i], bothCount);
 			
-			System.out.printf("The percentage of deli arrivals = %f %% (bCount = %d)\n", (double)deliCount/(normalCount+deliCount), deliCount);
+			prcDeli[i] = (double)deliCount/(normalCount+deliCount);
+			System.out.printf("The percentage of deli arrivals = %f %% (deliCount = %d)\n", prcDeli[i], deliCount);
 			System.out.printf("Total number of arrivals = %d\n", (normalCount+deliCount));
 			System.out.println();
 		}
@@ -82,13 +96,15 @@ public class InputDataModelling
 		double arrival = 0.0;  // initialise current arrival to 0
 		double nxtArrival;
 		double sumInterArrivals = 0.0;  // for computing the average
+		
+		double[] avgInterArrival = new double[16];
 		// For outputing to a file
 		PrintStream outFileStream;
 		try 
 		{
 			outFileStream = new PrintStream("data.csv");
 			outFileStream.printf("Arrival Time, Inter Arrival Time\n");
-			int cnt;
+			int cnt, i = 0;
 			for(cnt = 0; cnt < arrivals.size(); cnt++)
 			{
 				nxtArrival = arrivals.get(cnt);
@@ -97,7 +113,8 @@ public class InputDataModelling
 					sumInterArrivals += nxtArrival - arrival;
 				} else {
 					outFileStream.printf("%f,%f\n", nxtArrival, nxtArrival);
-					System.out.printf("Interarrival mean = %f, number of interarrival times = %d\n\n", sumInterArrivals/cnt, cnt);
+					avgInterArrival[i] = sumInterArrivals/(cnt/(i+1));
+					System.out.printf("Interarrival mean = %f, number of interarrival times = %d\n\n", avgInterArrival[i++], cnt);
 					sumInterArrivals = 0;
 				}
 				arrival = nxtArrival;
@@ -108,6 +125,31 @@ public class InputDataModelling
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
+		
+		String[] timeOfDay = {"9-9:30", "9:30-10", "10-10:30","10:30-11","11-11:30","11:30-12","12-12:30","12:30-1","1-1:30","1:30-2","2-2:30","2:30-3","3-3:30","3:30-4", "4-4:30","4:30-5"};
+		List<List<String>> sTats = new ArrayList<List<String>>();
+		for (int i = 0; i< 16; i++)
+		{
+			ArrayList<String> s = new ArrayList<String>();
+			s.add(timeOfDay[i]);
+			s.add(String.valueOf(Math.round(prcNorm[i]*10000)/100.0));
+			s.add(String.valueOf(Math.round(prcNormSingle[i]*10000)/100.0));
+			s.add(String.valueOf(Math.round(prcNormBoth[i]*10000)/100.0));
+			s.add(String.valueOf(Math.round(prcDeli[i]*10000)/100.0));
+			//s.add(String.valueOf(total[i]));
+			s.add(String.valueOf(Math.round(avgInterArrival[i]*100)/100.0));
+			sTats.add(s);
+		}
+		
+		Board boards = new Board(100);
+		List<String> columnHeaders = Arrays.asList("Time frame", "Normal Customers(%)", "Normal Singe Counter(%)", "Normal Both Counter(%)", "Deli Customers (%)", "Average InterArrival Time");
+		List<Integer> alignment = Arrays.asList(Block.DATA_BOTTOM_MIDDLE,Block.DATA_BOTTOM_MIDDLE,Block.DATA_BOTTOM_MIDDLE,Block.DATA_BOTTOM_MIDDLE, Block.DATA_BOTTOM_MIDDLE, Block.DATA_BOTTOM_MIDDLE);
+		Table tbl = new Table(boards, 100, columnHeaders, sTats);
+		tbl.setColAlignsList(alignment);
+		String tblString = boards.setInitialBlock(tbl.tableToBlocks()).build().getPreview();
+		System.out.println("The average percentage of normal customers is given below ");
+		System.out.println(tblString);
 	}
 
 }
